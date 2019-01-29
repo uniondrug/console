0fd663750fab7941b0b29d1c0d741aeb1bae0b37
付义兵 <websearch@163.com>
2019-01-29 16:51:27 +0800

    PHAR路径兼容Console

--- src/Console.php
+++ src/Console.php
@@ -1,12 +1,9 @@
 <?php
 /**
  * 命令行工具应用的容器。
- *
  * @author XueronNi <xueronni@uniondrug.cn>
  * @date   2018-01-16
- *
  */
-
 namespace Uniondrug\Console;
 
 use Exception;
@@ -40,9 +37,7 @@ class Console extends SymfonyApplication
                                           /____/   Console
 LOGO;
         parent::__construct($name, $version);
-
         restore_exception_handler();
-
         $this->registerCommands();
     }
 
@@ -53,69 +48,65 @@ LOGO;
             new ConfigCommand(),
             new MakeCommandCommand(),
         ]);
-
         // 通过配置文件定义的命令，可以将其他模块提供的命令添加进来
         $commands = $this->app->getConfig()->path('commands', []);
         $commands = array_unique($commands);
         foreach ($commands as $command) {
             $this->add(new $command());
         }
-
-        // 项目默认默认目录里面定义的工具
-        if (false !== ($files = glob($this->app->appPath() . '/Commands/*Command.php', GLOB_NOSORT | GLOB_NOESCAPE))) {
-            foreach ($files as $file) {
-                $command = '\\App\\Commands\\' . pathinfo($file, PATHINFO_FILENAME);
-                $this->add(new $command());
+        // 2019/01/29
+        // 扫描Command存储目录, 兼容PHAR部署
+        $cmdPath = $this->app->appPath().'/Commands';
+        if (is_dir($cmdPath)) {
+            $cmdHandle = dir($cmdPath);
+            while (false !== ($cmdEntry = $cmdHandle->read())) {
+                if (preg_match("/^(\S+Command)\.php/", $cmdEntry, $m) > 0) {
+                    $command = '\\App\\Commands\\'.$m[1];
+                    if (is_a($command, Command::class, true)) {
+                        $this->add(new $command());
+                    }
+                }
             }
+            $cmdHandle->close();
         }
     }
 
     /**
      * Get the default input definitions for the applications.
-     *
      * This is used to add the --env option to every available command.
-     *
      * @return \Symfony\Component\Console\Input\InputDefinition
      */
     protected function getDefaultInputDefinition()
     {
         $definition = parent::getDefaultInputDefinition();
-
         $definition->addOption($this->getEnvironmentOption());
-
         return $definition;
     }
 
     /**
      * Get the global environment option for the definition.
-     *
      * @return \Symfony\Component\Console\Input\InputOption
      */
     protected function getEnvironmentOption()
     {
         $message = 'The environment the command should run under.';
-
         return new InputOption('--env', '-e', InputOption::VALUE_OPTIONAL, $message, 'development');
     }
 
     /**
      * 重写doRun方法，出错时，日志记录
-     *
      * @param InputInterface  $input
      * @param OutputInterface $output
-     *
      * @return int
-     *
      * @throws \Throwable
      */
     public function doRun(InputInterface $input, OutputInterface $output)
     {
         try {
             return parent::doRun($input, $output);
-        } catch (\Exception $exception) {
+        } catch(\Exception $exception) {
             $this->handleException($exception);
         }
-
         throw $exception;
     }
 
@@ -127,16 +118,14 @@ LOGO;
         if (!$e instanceof Exception) {
             $e = new FatalThrowableError($e);
         }
-
         try {
             $trace = call_user_func($this->app->getConfig()->path('exception.log'), $e);
-        } catch (Exception $exception) {
+        } catch(Exception $exception) {
             $trace = [
                 'original' => explode("\n", $e->getTraceAsString()),
-                'handler'  => explode("\n", $exception->getTraceAsString()),
+                'handler' => explode("\n", $exception->getTraceAsString()),
             ];
         }
-
         $this->app->getLogger('console')->error($e->getMessage(), $trace);
     }
 }
